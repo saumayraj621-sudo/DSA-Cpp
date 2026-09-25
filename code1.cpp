24c1a7b3ba4b51a35c807306c6d6c3e70699798b
Saumay Raj <saumayraj621@gmail.com>
2026-09-25 18:00:30 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,6 @@
+#include <iostream> //preprocessor directive
+using namespace std; //using directive
+int main() {
+    cout << "Hello, World!" <<"raj" <<endl;
+    return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=10;
+    char b='z';
+    float c=10.5f;
+    bool issafe=true;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;\
+    cout<<issafe<<endl;
+
+    return 0;
+}
